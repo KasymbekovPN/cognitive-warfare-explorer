1ab724fd341a5067f239637d211950ffd83d83c3
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-04-23 16:28:12 +0300

    TestConfirmationMessage is move into mCommon

---
+++ backend/mCommon/src/main/java/ru/cwe/common/test/faker/MessageFaker.java
@@ -0,0 +1,44 @@
+package ru.cwe.common.test.faker;
+
+import lombok.RequiredArgsConstructor;
+
+// TODO: 23.04.2023 impl it
+@RequiredArgsConstructor
+public final class MessageFaker {
+	private final BaseFaker base;
+
+
+
+	/*
+
+	@RequiredArgsConstructor
+public final class MessageFaker {
+	private final BaseFaker base;
+
+	public int version(){
+		return base.number().between(Versions.MIN, Versions.MAX + 1);
+	}
+
+	public int priority(){
+		return base.number().between(Priorities.MIN, Priorities.MAX + 1);
+	}
+
+	public MessageType messageType(){
+		return MessageType.valueOf(
+			base.number().between(
+				MessageType.INVALID.getValue(),
+				MessageType.CONFIRMATION.getValue() + 1)
+		);
+	}
+
+	public ConfirmationResult confirmationResult(){
+		return ConfirmationResult.valueOf(
+			base.number().between(
+				ConfirmationResult.INVALID.getValue(),
+				ConfirmationResult.RESPONSE.getValue() + 1)
+		);
+	}
+}
+
+	 */
+}
--- backend/mCommon/src/main/java/ru/cwe/common/test/message/TestConfirmationMessage.java
+++ backend/mCommon/src/main/java/ru/cwe/common/test/message/TestConfirmationMessage.java
@@ -1,6 +1,7 @@
 package ru.cwe.common.test.message;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import ru.cwe.common.message.MessageType;
 import ru.cwe.common.message.confirmation.ConfirmationMessage;
@@ -8,9 +9,9 @@ import ru.cwe.common.message.confirmation.ConfirmationResult;
 
 import java.util.UUID;
 
-// TODO: 22.04.2023 add test
 @RequiredArgsConstructor
 @EqualsAndHashCode
+@Getter
 public class TestConfirmationMessage implements ConfirmationMessage {
 	private final int version;
 	private final int priority;
@@ -18,34 +19,4 @@ public class TestConfirmationMessage implements ConfirmationMessage {
 	private final MessageType type = MessageType.CONFIRMATION;
 	private final ConfirmationResult result;
 	private final String payloadMessageType;
-
-	@Override
-	public int getVersion() {
-		return -1;
-	}
-
-	@Override
-	public int getPriority() {
-		return -1;
-	}
-
-	@Override
-	public MessageType getType() {
-		return null;
-	}
-
-	@Override
-	public UUID getUuid() {
-		return null;
-	}
-
-	@Override
-	public ConfirmationResult getResult() {
-		return null;
-	}
-
-	@Override
-	public String getPayloadMessageType() {
-		return null;
-	}
 }
